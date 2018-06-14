e851297fcac783c4335b5954e432f84904cc79c3
beqa <btsalani@emis.ge>
2018-06-14 13:11:39 +0400

    added main menu

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu, MenuItem } = require('electron');
 
 function createWindow(){
 	let win = new BrowserWindow();
+
+	const menuTemplate = [
+		{
+			label: 'File',
+			submenu: [
+				{label: 'New Project'},
+				{label: 'Exit', role: 'close'}
+			]
+		},
+		{
+			label: 'Help',
+			submenu: [
+				{label: 'About'}
+			]
+		}
+	];
+
+	let menu = Menu.buildFromTemplate(menuTemplate);
+
+	win.setMenu(menu);
 }
 
 app.on('ready',createWindow);
