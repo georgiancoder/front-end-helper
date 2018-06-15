3a8398947a821d6826fa28e9c80001531188ac4e
beqa <btsalani@emis.ge>
2018-06-15 11:20:49 +0400

    list all files in project directory

--- index.js
+++ index.js
@@ -5,56 +5,64 @@ let mainWindow;
 let aboutWindow;
 let newProjectWindow;
 
-function createWindow(){
-	
-	mainWindow = new BrowserWindow();
-	const menuTemplate = [
-		{
-			label: 'File',
-			submenu: [
-				{label: 'New Project', click() { newProject() }},
-				{label: 'Exit', role: 'close'}
-			]
-		},
-		{
-			label: 'Help',
-			submenu: [
-				{label: 'About', click() { About(); }}
-			]
-		}
-	];
+function createWindow() {
 
-	let menu = Menu.buildFromTemplate(menuTemplate);
+    mainWindow = new BrowserWindow();
+    const menuTemplate = [{
+            label: 'File',
+            submenu: [
+                { label: 'New Project', click() { newProject() } },
+                { label: 'Exit', role: 'close' }
+            ]
+        },
+        {
+            label: 'Help',
+            submenu: [
+                { label: 'About', click() { About(); } }
+            ]
+        }
+    ];
 
-	mainWindow.setMenu(menu);
+    let menu = Menu.buildFromTemplate(menuTemplate);
 
-	mainWindow.loadURL(`file://${__dirname}/views/index.html`);
-	mainWindow.on('close',closeApp);
+    mainWindow.setMenu(menu);
+
+    mainWindow.loadURL(`file://${__dirname}/views/index.html`);
+    mainWindow.on('close', closeApp);
 }
 
 
-function About(){
-	aboutWindow = new BrowserWindow({parent: 'top', width: 300, height: 300, frame: false});
-	aboutWindow.setMenuBarVisibility(false);
-	aboutWindow.loadURL(`file://${__dirname}/views/about.html`);
-	aboutWindow.on('blur',()=>{
-		aboutWindow.close();
-	})
+function About() {
+    aboutWindow = new BrowserWindow({ parent: 'top', width: 300, height: 300, frame: false });
+    aboutWindow.setMenuBarVisibility(false);
+    aboutWindow.loadURL(`file://${__dirname}/views/about.html`);
+    aboutWindow.on('blur', () => {
+        aboutWindow.close();
+    })
 }
 
-function newProject(){
-	let projectfolder = dialog.showOpenDialog({properties: ['openDirectory', 'multiSelections']})
-	fs.readdir(projectfolder[0],(err,data)=>{
-		if(err){
-			console.log(err);
-		} else {
-			console.log(data);
-		}
-	})
+function newProject() {
+    let projectfolder = dialog.showOpenDialog({ properties: ['openDirectory', 'multiSelections'] })
+    let fileList = [];
+    readDirectory(projectfolder[0], fileList);
+    console.log(fileList.length);
 }
 
-function closeApp(){
-	aboutWindow.close();
+function readDirectory(path, list) {
+    let files = fs.readdirSync(path);
+    files.forEach((file) => {
+        let filePath = `${path}\\${file}`;
+        let fileStat = fs.lstatSync(filePath);
+        if (fileStat.isDirectory()) {
+            readDirectory(filePath, list);
+        } else {
+            list.push(filePath);
+        }
+    });
 }
 
-app.on('ready',createWindow);
+function closeApp() {
+    aboutWindow.close();
+}
+
+app.on('ready', createWindow);
