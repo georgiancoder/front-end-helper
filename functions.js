2d6ca6fd0dbbc55913e750747803cfda0be134f8
Beka Tsalani <beqacalani@gmail.com>
2018-06-16 17:28:08 +0400

    separate function into another functions class file

---
+++ functions.js
@@ -0,0 +1,74 @@
+const { dialog } = require('electron');
+const fs = require('fs');
+
+class Functions {
+    newProject(projectList) {
+        let projectfolder = dialog.showOpenDialog({ properties: ['openDirectory'] })
+        let fileList = [];
+        if (projectfolder) {
+            this.createMainConfig(projectfolder[0], projectList);
+            this.readDirectory(projectfolder[0], fileList);
+            fileList = fileList.filter((item) => {
+                if (item.match(/[^.]*$/)[0] == "html") {
+                    return item;
+                }
+            });
+        }
+    }
+
+
+    createMainConfig(path, projectList) {
+        let directoryName = path.match(/[^\\]*$/)[0];
+        let projectsConfig = `projects.json`;
+        if (fs.existsSync(projectsConfig)) {
+            projectList = fs.readFileSync(projectsConfig).toString();
+            projectList = projectList.length>0 ? JSON.parse(projectList) : [];
+            if (this.isInProjects(projectList, path)) {
+                dialog.showMessageBox({message: 'Project already exists!'});
+            } else {
+                projectList.push({
+                    "name": directoryName,
+                    "path": path
+                });
+                fs.writeFileSync(projectsConfig, JSON.stringify(projectList));
+            }
+        } else {
+            projectList.push({
+                "name": directoryName,
+                "path": path
+            });
+            fs.writeFileSync(projectsConfig, JSON.stringify(projectList));
+        }
+    }
+
+    isInProjects(projects, path) {
+        let is = false;
+        projects.forEach(project => {
+            if (path.indexOf(project.path) >= 0) {
+                is = true;
+            }
+        });
+        return is;
+    }
+
+    readDirectory(path, list) {
+        let files = fs.readdirSync(path);
+        files.forEach((file) => {
+            let filePath = `${path}\\${file}`;
+            let fileStat = fs.lstatSync(filePath);
+            if (fileStat.isDirectory()) {
+                this.readDirectory(filePath, list);
+            } else {
+                list.push(filePath);
+            }
+        });
+    }
+
+    closeApp() {
+        if(aboutWindow){
+            aboutWindow.close();
+        }
+    }
+}
+
+module.exports = new Functions();
